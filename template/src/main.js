33168ace11774989b8a3ab3176eb6336561cef50
ChaselWu <minowu@foxmail.com>
2017-01-30 20:08:51 +0800

    Set main.js

--- template/src/main.js
+++ template/src/main.js
@@ -3,16 +3,24 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import App from './app'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import human from './human'
+import env from './env'
+import store from './store'
+import router from './router'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  env,
+  human,
+  store,
+  router,
   {{#if_eq build "runtime"}}
   render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   {{/if_eq}}
   {{#if_eq build "standalone"}}
-  template: '<App/>',
+  template: '<app></app>',
   components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   {{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
