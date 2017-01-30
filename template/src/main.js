30d800643daac6e9dead96007c097e6268273610
ChaselWu <minowu@foxmail.com>
2017-01-31 01:55:11 +0800

    Set main.js

--- template/src/main.js
+++ template/src/main.js
@@ -1,9 +1,5 @@
-{{#if_eq build "standalone"}}
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-{{/if_eq}}
-import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import App from './app'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Vue from 'vue'
+import App from './app'
 import human from './human'
 import env from './env'
 import store from './store'
@@ -11,16 +7,10 @@ import router from './router'
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: 'app',
   env,
   human,
   store,
   router,
-  {{#if_eq build "runtime"}}
-  render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
-  {{#if_eq build "standalone"}}
-  template: '<app></app>',
-  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  render: h => h(App)
+})
