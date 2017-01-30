81cc403705fa5abb0ff7abee0adf3d0a6437e904
ChaselWu <minowu@foxmail.com>
2017-01-31 01:54:55 +0800

    Using 2.0.0-rc.4

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -25,8 +25,7 @@ module.exports = {
     alias: {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components'),
-      'vue-human': path.resolve(__dirname, '../node_modules/vue-human/src')
+      'components': path.resolve(__dirname, '../src/components')
     }
   },
   resolveLoader: {
