ec503a178ee16847417bd2ec9370ff8b9050b7f8
ChaselWu <minowu@foxmail.com>
2017-11-29 21:25:18 +0800

    Update options

--- template/scripts/configs/options.js
+++ template/scripts/configs/options.js
@@ -1,8 +1,4 @@
-const path = require('path')
-
 module.exports = {
-  moduleDirectory: path.join(process.cwd(), 'example'),
-  sassResources: path.join(process.cwd(), 'example/src/human/config.scss'),
-  // @todo Add multi example test to test publicPath
-  publicPath: '/house/'
+  // If you want to build child folder
+  // publicPath: '/app/'
 }
