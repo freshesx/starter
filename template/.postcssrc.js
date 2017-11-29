5c1451878f2077d4ac36bf7a2a0d61c41f3ef75a
ChaselWu <minowu@foxmail.com>
2017-11-29 20:07:50 +0800

    Update postcssrc

--- template/.postcssrc.js
+++ template/.postcssrc.js
@@ -1,8 +1,16 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-
 module.exports = {
   "plugins": {
-    // to edit target browsers: use "browserlist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {
+      "browsers": [
+        "defaults",
+        "not ie <= 100",
+        "not ExplorerMobile <= 100",
+        "not Opera <=100",
+        "not OperaMini <= 100",
+        "not OperaMini all",
+        "not OperaMobile <= 100",
+        "not BlackBerry <= 100"
+      ]
+    }
   }
 }
