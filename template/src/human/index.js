c781ff3bcf77f8db6cc0f0a799b558416a9d8a15
ChaselWu <minowu@foxmail.com>
2017-11-29 20:17:55 +0800

    Update main.js and human

--- template/src/human/index.js
+++ template/src/human/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
-import Human from 'vue-human'
-import locales from '../locale'
+import human from 'vue-human'
 import core from 'vue-human/suites/core'
 import './app.scss'
 
-// Use Human
-Vue.use(Human, { locales })
+// Use human
+Vue.use(human)
 Vue.use(core)
+
+export default human
--- template/src/main.js
+++ template/src/main.js
@@ -1,18 +1,15 @@
 import 'babel-polyfill'
 import Vue from 'vue'
-import env from './env'
 import store from './store'
-import './human'
-import App from './app'
+import human from './human'
+import app from './app'
 import router from './router'
-
-Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  env,
   store,
   router,
-  render: h => h(App)
+  human
+  render: h => h(app)
 })
